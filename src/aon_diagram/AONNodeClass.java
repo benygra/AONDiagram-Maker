1179b8cb0528fe7a873ae9e568488c5560c7c8b8
Bernardo <b.gracioso@campus.fct.unl.pt>
2024-09-26 20:57:30 +0100

    Adding instance variables and constructor to AONNodeClass

--- src/aon_diagram/AONNodeClass.java
+++ src/aon_diagram/AONNodeClass.java
@@ -1,4 +1,33 @@
 package aon_diagram;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AONNodeClass implements AONNode {
+
+    private final int duration;
+    private final String name;
+
+    private int earlyStart;
+    private int earlyFinish;
+    private int lateStart;
+    private int lateFinish;
+    private int slack;
+
+    private Map<String, AONNode> parents;
+    private Map<String, AONNode> children;
+
+    public AONNodeClass(int duration, String name) {
+        this.duration = duration;
+        this.name = name;
+
+        this.earlyStart = 0;
+        this.earlyFinish = 0;
+        this.lateStart = 0;
+        this.lateFinish = 0;
+        this.slack = 0;
+
+        this.parents = new HashMap<>();
+        this.children = new HashMap<>();
+    }
 }
